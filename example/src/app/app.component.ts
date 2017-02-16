f711c40f4f5b9b6c47f9b5d85d7ddcbd194b1fd8
Janne Julkunen <janne.julkunen@zef.fi>
2017-02-16 16:10:36 +0200

    Added systemjs build system for the example app

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -6,9 +6,10 @@ export class Cmyk {
 }
 
 @Component({
+    moduleId: module.id + '',
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
